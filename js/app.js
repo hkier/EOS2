f3f6fb705d7e6e2b7632c6081da62ed9e0283be8
Howard Kier <howard@Howards-Air.hsd1.il.comcast.net>
2017-08-05 22:11:17 -0500

    Can pull IPTC data

--- js/app.js
+++ js/app.js
@@ -5,16 +5,20 @@ var chalk =require('chalk');
 fs.readFile('0098pp.jpg', function(err,data){
     if (err) {throw err}
     console.log (chalk.red('\nFile: 0098pp.jpg'));
-         console.log('Data:', iptc(data));
-    console.log('Data[245]: ',data[245]);
-    console.log(chalk.blue('Keys:', data.keys   ,'\n'));
+    // console.log(chalk.green('data: ', data.length));
+    const aaa =iptc(data)
+    console.log('headline:', aaa.headline);
+    console.log('special_instructions: ', aaa.special_instructions)
 
 });
 
-// fs.readFile('0015.jpg', function(err,data){
-//     if (err) {throw err}
-//         console.log (chalk.red('0015.jpg'));
-//         console.log(iptc(data));
+fs.readFile('0015.jpg', function(err,data){
+    if (err) {throw err}
+        console.log (chalk.red('\nFile: 0015.jpg'));
+
+const bbb =iptc(data)
+    console.log('headline:', bbb.headline);
+    console.log('special_instructions: ', bbb.special_instructions)
 
 
-// });
+});
